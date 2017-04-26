3d23632c2e80ff43e7f04c614fc660ea8e410f4d
labanlin1 <labanlin@me.com>
2017-04-26 15:09:09 -0400

    Basic Functionality Added
    Need to test on user device to sample heights

--- demos/DayToDay/index.js
+++ demos/DayToDay/index.js
@@ -2,12 +2,31 @@
  * Created by Laban on 4/25/2017.
  */
 //Default Variables
-var tabsContainer = document.querySelector("tabs");
-loadAndParseJSON("/modules/restaurant.json");
+var tabsContainer = document.querySelector(".tabs");
+var tabsObjectsContainer = document.querySelector(".tab-objects");
+var phraseContainer = document.querySelector(".phrase");
+var programmedOptionsContainer = document.querySelector("programmed-options");
+var textMatchOptionsContainer = document.querySelector("text-match-options");
+var transcriptContainer = document.querySelector("#transcript-container");
+var transcriptToggle = document.querySelector(".transcript");
+loadAndParseJSON("https://api.myjson.com/bins/yet63");
+getLastField().focus();
+initializeSuggestions();
+document.onkeypress = function (e) {
+    captureEnter(e);
+};
+document.onkeyup = function (e) {
+    captureBackspace(e);
+};
+transcriptToggle.addEventListener("click", toggleTranscript);
 var Template = (function () {
     function Template(text, suggestedWords) {
+        this.alternateSelected = 0;
         this.text = text;
         this.suggestedWords = suggestedWords;
+        var optionsFinder = /\|([A-Za-z,]+)\|/;
+        this.alternates = optionsFinder.exec(this.text)[1].split(",");
+        this.display = text.replace(optionsFinder, "<span class = 'emphasis'>" + this.alternates[this.alternateSelected]) + "</span>";
     }
     return Template;
 }());
@@ -19,32 +38,167 @@ var Word = (function () {
 }());
 //Template Functionality
 function addTemplateToPhrase(template) {
+    //Insert Template
+    var newTemplateInstance = document.createElement("span");
+    newTemplateInstance.classList.add("template");
+    newTemplateInstance.innerHTML = template.display;
+    phraseContainer.insertBefore(newTemplateInstance, getLastField());
+    newTemplateInstance.addEventListener("dblclick", removeTemplateFromPhrase);
+    getLastField().focus(); //Webstorm says this doesn't work, but it totally does.
+    //Update Suggested Tab
+    updateProgrammableSugggestions(template);
 }
-function removeTemplateFromPhrase() {
+function removeTemplateFromPhrase(e) {
+    var target = e.target;
+    phraseContainer.removeChild(target);
 }
-function removeLastWord() {
+function updateProgrammableSugggestions(template) {
+    var programmableSuggestions = document.querySelectorAll(".programmed-options .option");
+    console.log(programmableSuggestions);
+    for (var i = 0; i < Math.min(3, template.suggestedWords.length); i++) {
+        programmableSuggestions[i].innerHTML = template.suggestedWords[i].word;
+    }
 }
+//JSON Functions
 function loadAndParseJSON(url) {
     retrieveJson(url);
 }
 function retrieveJson(url) {
     //http://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
-    console.log("retrieving JSON");
     var json = new XMLHttpRequest();
-    console.log("retrieving JSON");
     json.overrideMimeType("application/json");
     json.open("GET", url, true);
-    console.log("retrieving JSON");
+    json.send();
     json.onreadystatechange = function () {
-        console.log("retrieving JSON");
-        console.log(json.readyState);
-        console.log(json.status);
         if (json.readyState == 4 && json.status == 200) {
             parseJSONIntoTemplates(json.responseText);
         }
     };
 }
 function parseJSONIntoTemplates(json) {
-    console.log(json);
+    var jsonObject = JSON.parse(json);
+    console.log(jsonObject);
+    console.log(jsonObject.moduleName);
+    //Create New Tab
+    var newTab = document.createElement("div");
+    newTab.classList.add("tab");
+    newTab.appendChild(document.createTextNode(jsonObject.moduleName));
+    tabsContainer.appendChild(newTab);
+    console.log("New Tab: " + jsonObject.moduleName + " added successfully.");
+    //Create New Objects
+    var newContainer = document.createElement("div");
+    newContainer.classList.add("constructors-container");
+    var _loop_1 = function(i) {
+        var t = jsonObject.templates[i];
+        var option = new Template(t.text, t.suggestedWords);
+        //Wrap Option in appropriate markup
+        var newOption = document.createElement("div");
+        newOption.classList.add("option");
+        newOption.setAttribute("module", jsonObject.moduleName);
+        newOption.innerHTML = option.display;
+        newOption.addEventListener("click", function () {
+            addTemplateToPhrase(option);
+        });
+        newOption.addEventListener("touchend", function () {
+            addTemplateToPhrase(option);
+        });
+        newContainer.appendChild(newOption);
+    };
+    for (var i = 0; i < jsonObject.templates.length; i++) {
+        _loop_1(i);
+    }
+    tabsObjectsContainer.appendChild(newContainer);
+    newContainer.classList.add("active");
+    setAsActiveTab(newTab);
+}
+function setAsActiveTab(tab) {
+    var activeTab = document.querySelector(".tab.active");
+    activeTab.classList.remove("active");
+    tab.classList.add("active");
+}
+//Helper Functions
+function getLastField() {
+    var elements = document.querySelectorAll(".field");
+    if (elements[elements.length - 1].innerHTML == "") {
+        return elements[elements.length - 1];
+    }
+    else {
+        var newField = createField();
+        phraseContainer.appendChild(newField);
+        return newField;
+    }
+}
+function createField() {
+    var element = document.createElement("span");
+    element.setAttribute("contenteditable", "true");
+    element.classList.add("field");
+    return element;
+}
+function addSuggestionToPhrase(e) {
+    var text = e.target.innerHTML;
+    //Insert Template
+    var newTemplateInstance = document.createElement("span");
+    newTemplateInstance.classList.add("template");
+    newTemplateInstance.innerHTML = text;
+    phraseContainer.insertBefore(newTemplateInstance, getLastField());
+    newTemplateInstance.addEventListener("dblclick", removeTemplateFromPhrase);
+    getLastField().focus(); //Webstorm says this doesn't work, but it totally does.
+    //Update Suggested Tab
+}
+function initializeSuggestions() {
+    var suggestions = document.querySelectorAll(".programmed-options .option");
+    for (var i = 0; i < suggestions.length; i++) {
+        suggestions[i].addEventListener("click", addSuggestionToPhrase);
+    }
+}
+function captureEnter(e) {
+    var keyID = e.keyCode;
+    if (keyID == 13) {
+        e.preventDefault();
+        addToTranscript(phraseContainer.innerHTML);
+        phraseContainer.innerHTML = "";
+        var newField = createField();
+        phraseContainer.appendChild(newField);
+        newField.focus();
+    }
+}
+function captureBackspace(e) {
+    var keyID = e.keyCode;
+    //8 => backspace, 46 => delete
+    if (keyID == 46) {
+        var activeElement = document.activeElement;
+        if (activeElement.classList.contains("field")) {
+            if (activeElement.textContent == "") {
+                removeLastWord(activeElement.previousElementSibling);
+            }
+        }
+    }
+}
+function removeLastWord(element) {
+    if (element) {
+        var text = element.innerHTML;
+        var lastIndex = text.lastIndexOf(" ");
+        text = text.substring(0, lastIndex);
+        element.innerHTML = text;
+        if (text == "" || text == " ") {
+            element.parentNode.removeChild(element);
+        }
+    }
+}
+function addToTranscript(log) {
+    var htmlMarkupRemover = /<(?:\/)?[A-Za-z ="']*>/ig;
+    var newEntry = log.replace(htmlMarkupRemover, "");
+    var newEntryP = document.createElement("p");
+    newEntryP.textContent = newEntry;
+    transcriptContainer.appendChild(newEntryP);
+}
+function toggleTranscript() {
+    if (transcriptContainer.classList.contains("visible")) {
+        transcriptContainer.classList.remove("visible");
+    }
+    else {
+        transcriptContainer.classList.add("visible");
+        transcriptContainer.scrollTop = transcriptContainer.clientHeight;
+    }
 }
 //# sourceMappingURL=index.js.map
--- demos/DayToDay/index.ts
+++ demos/DayToDay/index.ts
@@ -4,16 +4,40 @@
 
 //Default Variables
 
-const tabsContainer = document.querySelector("tabs");
+let tabsContainer = document.querySelector(".tabs");
+let tabsObjectsContainer = document.querySelector(".tab-objects");
+let phraseContainer = document.querySelector(".phrase");
+let programmedOptionsContainer = document.querySelector("programmed-options");
+let textMatchOptionsContainer = document.querySelector("text-match-options");
+let transcriptContainer = document.querySelector("#transcript-container");
+let transcriptToggle = document.querySelector(".transcript");
 
-loadAndParseJSON("/modules/restaurant.json");
 
+loadAndParseJSON("https://api.myjson.com/bins/yet63");
+getLastField().focus();
+initializeSuggestions();
+document.onkeypress = function(e){
+    captureEnter(e);
+};
+
+document.onkeyup = function(e){
+    captureBackspace(e);
+};
+
+transcriptToggle.addEventListener("click", toggleTranscript);
 class Template{
     text:string;
+    display:string;
+    alternates:string[];
+    alternateSelected: number;
     suggestedWords:Word[];
     constructor(text:string, suggestedWords:Word[]){
+        this.alternateSelected=0;
         this.text = text;
         this.suggestedWords = suggestedWords;
+        const optionsFinder = /\|([A-Za-z,]+)\|/;
+        this.alternates = optionsFinder.exec(this.text)[1].split(",");
+        this.display = text.replace(optionsFinder, "<span class = 'emphasis'>" + this.alternates[this.alternateSelected]) + "</span>";
     }
 }
 
@@ -26,16 +50,32 @@ class Word{
 //Template Functionality
 
 function addTemplateToPhrase(template:Template){
+    //Insert Template
+    let newTemplateInstance = document.createElement("span");
+    newTemplateInstance.classList.add("template");
+    newTemplateInstance.innerHTML = template.display;
+    phraseContainer.insertBefore(newTemplateInstance,getLastField());
+    newTemplateInstance.addEventListener("dblclick", removeTemplateFromPhrase);
+    getLastField().focus(); //Webstorm says this doesn't work, but it totally does.
 
+    //Update Suggested Tab
+    updateProgrammableSugggestions(template);
 }
 
-function removeTemplateFromPhrase(){
-
+function removeTemplateFromPhrase(e){
+    let target = e.target;
+    phraseContainer.removeChild(target);
 }
 
-function removeLastWord(){
-
+function updateProgrammableSugggestions(template:Template){
+    let programmableSuggestions = document.querySelectorAll(".programmed-options .option");
+    console.log(programmableSuggestions);
+    for (let i = 0; i<Math.min(3,template.suggestedWords.length); i++){
+        programmableSuggestions[i].innerHTML = template.suggestedWords[i].word;
+    }
 }
+
+//JSON Functions
 
 function loadAndParseJSON(url:string){
     retrieveJson(url);
@@ -43,22 +83,154 @@ function loadAndParseJSON(url:string){
 
 function retrieveJson(url:string){
     //http://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
-    console.log("retrieving JSON");
     let json = new XMLHttpRequest();
-    console.log("retrieving JSON");
     json.overrideMimeType("application/json");
     json.open("GET", url, true);
-    console.log("retrieving JSON");
+    json.send();
     json.onreadystatechange = function(){
-        console.log("retrieving JSON");
-        console.log(json.readyState);
-        console.log(json.status);
       if (json.readyState == 4 && json.status == 200){
           parseJSONIntoTemplates(json.responseText);
       }
     };
 
 }
+
 function parseJSONIntoTemplates(json){
-    console.log(json);
+    var jsonObject = JSON.parse(json);
+    console.log(jsonObject);
+    console.log(jsonObject.moduleName);
+
+    //Create New Tab
+    var newTab = document.createElement("div");
+    newTab.classList.add("tab");
+    newTab.appendChild(document.createTextNode(jsonObject.moduleName));
+    tabsContainer.appendChild(newTab);
+
+    console.log("New Tab: " + jsonObject.moduleName + " added successfully.");
+
+    //Create New Objects
+    let newContainer = document.createElement("div");
+    newContainer.classList.add("constructors-container");
+
+    for (let i = 0; i<jsonObject.templates.length; i++){
+        const t = jsonObject.templates[i];
+        let option = new Template(t.text, t.suggestedWords);
+        //Wrap Option in appropriate markup
+        let newOption = document.createElement("div");
+        newOption.classList.add("option");
+        newOption.setAttribute("module", jsonObject.moduleName);
+        newOption.innerHTML = option.display;
+        newOption.addEventListener("click", function(){
+            addTemplateToPhrase(option);
+        });
+        newOption.addEventListener("touchend", function(){
+            addTemplateToPhrase(option);
+        });
+        newContainer.appendChild(newOption);
+
+    }
+    tabsObjectsContainer.appendChild(newContainer);
+    newContainer.classList.add("active");
+
+    setAsActiveTab(newTab);
+}
+
+function setAsActiveTab(tab){
+    let activeTab = document.querySelector(".tab.active");
+    activeTab.classList.remove("active");
+    tab.classList.add("active");
+}
+
+//Helper Functions
+
+function getLastField(){
+    const elements = document.querySelectorAll(".field");
+    if(elements[elements.length-1].innerHTML == ""){
+        return elements[elements.length-1];
+    }else{
+        let newField = createField();
+        phraseContainer.appendChild(newField);
+        return newField;
+    }
+}
+
+function createField(){
+    let element = document.createElement("span");
+    element.setAttribute("contenteditable", "true");
+    element.classList.add("field");
+    return element;
+}
+
+function addSuggestionToPhrase(e){
+    const text = e.target.innerHTML;
+    //Insert Template
+    let newTemplateInstance = document.createElement("span");
+    newTemplateInstance.classList.add("template");
+    newTemplateInstance.innerHTML = text;
+    phraseContainer.insertBefore(newTemplateInstance,getLastField());
+    newTemplateInstance.addEventListener("dblclick", removeTemplateFromPhrase);
+    getLastField().focus(); //Webstorm says this doesn't work, but it totally does.
+
+    //Update Suggested Tab
+}
+
+function initializeSuggestions(){
+    const suggestions = document.querySelectorAll(".programmed-options .option");
+    for (let i = 0; i<suggestions.length; i++){
+        suggestions[i].addEventListener("click", addSuggestionToPhrase);
+    }
+}
+
+function captureEnter(e){
+    let keyID = e.keyCode;
+    if (keyID == 13){
+        e.preventDefault();
+        addToTranscript(phraseContainer.innerHTML);
+        phraseContainer.innerHTML="";
+        let newField = createField();
+        phraseContainer.appendChild(newField);
+        newField.focus();
+    }
+}
+
+function captureBackspace(e){
+    let keyID = e.keyCode;
+    //8 => backspace, 46 => delete
+    if (keyID == 46){
+        const activeElement = document.activeElement;
+        if (activeElement.classList.contains("field")){
+            if (activeElement.textContent == ""){
+                removeLastWord(activeElement.previousElementSibling);
+            }
+        }
+    }
+}
+
+function removeLastWord(element){
+    if (element) {
+        let text = element.innerHTML;
+        let lastIndex = text.lastIndexOf(" ");
+        text = text.substring(0, lastIndex);
+        element.innerHTML = text;
+        if (text == "" || text == " ") {
+            element.parentNode.removeChild(element);
+        }
+    }
+}
+
+function addToTranscript(log:string){
+    const htmlMarkupRemover = /<(?:\/)?[A-Za-z ="']*>/ig;
+    let newEntry = log.replace(htmlMarkupRemover,"");
+    let newEntryP = document.createElement("p");
+    newEntryP.textContent = newEntry;
+    transcriptContainer.appendChild(newEntryP);
+}
+
+function toggleTranscript(){
+    if (transcriptContainer.classList.contains("visible")){
+        transcriptContainer.classList.remove("visible");
+    }else{
+        transcriptContainer.classList.add("visible");
+        transcriptContainer.scrollTop = transcriptContainer.clientHeight;
+    }
 }
